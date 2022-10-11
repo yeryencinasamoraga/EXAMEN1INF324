8156514a9dd296bb39764692ff6cb5713c45f60c
yeryencinasamoraga <115434248+yeryencinasamoraga@users.noreply.github.com>
2022-10-11 10:22:27 -0400

    Add files via upload

---
+++ Pregunta_1/qmc/index.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>FACULTAD DE CIENCIAS PURAS Y NATURALES</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="../css/font-awesome.min.css">
+  <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+
+
+<header class="header">
+  <div class="container">
+    <div class="row justify-content-between align-items-center">
+      <div class="logo">
+        <a href="../index.php">FACULTAD DE CIENCIAS PURAS Y NATURALES</a>
+      </div>
+      <input type="checkbox" id="nav-check">
+      <label for="nav-check" class="nav-toggler">
+        <span></span>
+      </label>
+      <nav class="nav">
+        <ul>
+          <li><a href="../index.php">INICIO</a></li>
+          <li><a href="#courses">OTRAS CARRERAS</a></li>
+          <li><a href="#reviews">AYUDA</a></li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+</header>
+
+<section class="home-section" id="home">
+  <div class="container">
+    <div class="row h-100 align-items-center align-content-center">
+      <div class="home-text">
+        <h1>QUIMICA   <span>FCPN</span><br> MISION</h1>
+        <h2>
+        La Carrera de Ciencias Químicas de la Universidad Mayor de San Andrés 
+        es una institución de educación superior pública y gratuita, que dentro
+         del marco de la Autonomía y el cogobiemo, tiene la misión de formar 
+         recursos humanos en Ciencias Químicas de elevada calificación, competitividad 
+         profesional, creatividad, adaptación, con valores éticos, la suficiente 
+         conciencia social y vocación de servicio para responder a las necesidades
+         de la población, al desarrollo local, regional y nacional.
+
+
+        </h2>
+        <a href="#" class="btn btn-1">Conoce Mas Sobre  la Carrera de Quimica</a>
+      </div>
+      <div class="home-img">
+        <div class="home-img-inner">
+          
+          <img src="../img/qmc.png" alt="home-img">
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="courses-section sec-padding" id="courses">
+  <div class="container">
+    <div class="row">
+      <div class="section-title">
+        <h2><span>CARRERAS</span></h2>
+      </div>
+    </div>
+
+    <div class="row">
+        
+      <div class="course-item">
+        <a href="../inf/index.php">
+          <div class="course-img">
+             <img src="../img/inf.png" alt="course-img">
+             <div class="course-price">
+              INFORMATICA
+            </div>
+          </div>
+          <div class="course-info">
+             <ul>
+               <li>Monoblock Central 2do Patio</li>
+             </ul>
+             
+          </div>
+        </a>
+      </div>
+      <div class="course-item">
+        <a href="../fis/index.php">
+          <div class="course-img">
+             <img src="../img/fisica.jpg" alt="course-img">
+             <div class="course-price">
+               FISICA
+             </div>
+          </div>
+          <div class="course-info">
+             <ul>
+               <li>Campus UMSA CotaCota</li>
+    
+             </ul>
+           
+          </div>
+        </a>
+      </div>
+      <div class="course-item">
+        <a href="#">
+          <div class="course-img">
+             <img src="../img/biologia.jpg" alt="course-img">
+             <div class="course-price">
+              BIOLOGIA
+            </div>
+          </div>
+          <div class="course-info">
+             <ul>
+               <li>Campus UMSA CotaCota</li>
+              
+             </ul>
+             
+          </div>
+        </a>
+      </div>
+    </div>
+  </div>
+</section>
+
+<footer class="footer">
+  <div class="container">
+    <div class="row">
+      <div class="footer-item footer-about">
+        <h2>Nosotros</h2>
+        <p>La plataforma presente es perteneciente a la UMSA la cual esta hecha para una mejor comunicacion entre docentes y estudiantes asi
+           para lograr ver sus calificaciones y conocer acerca de nuestra Facultad.</p>
+        <div class="social-links">
+          <a href="#"><i class="fab fa-facebook-f"></i></a>
+          <a href="#"><i class="fab fa-twitter"></i></a>
+          <a href="#"><i class="fab fa-instagram"></i></a>
+          <a href="#"><i class="fab fa-youtube"></i></a>
+        </div>
+      </div>
+      
+      <div class="footer-item footer-contact">
+        <h2>Contactenos</h2>
+        <p><i class="fas fa-map-marker-alt"></i> Monoblock Central (Edificio Antiguo) </p>
+        <p><i class="fas fa-phone"></i> 123 - 456 -789</p>
+        <p><i class="fas fa-envelope"></i> yyyyyyyy@fcpn.edu.com</p>
+        
+      </div>
+      
+    </div>
+  </div>
+</footer>
+</body>
+</html>
